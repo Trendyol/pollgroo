daf4597c2b0738bc608d267bb66928ca8444fed4
Deniz Akpınar <ddenizakpinar@gmail.com>
2023-01-26 19:46:01 +0300

    refactor(ui): update tailwind content

--- packages/ui/tailwind.config.js
+++ packages/ui/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./button/**/*.{js,ts,jsx,tsx}'],
+  content: ['./*/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
