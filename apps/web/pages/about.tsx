78b06967677474ab5eab0d55f6cdf25d80aea8bc
Armağan Dalkıran <armagandalkran20@gmail.com>
2023-03-18 01:58:51 +0300

    test

--- apps/web/pages/about.tsx
+++ apps/web/pages/about.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/../../packages/ui";
+
 export default function AboutPage() {
   return (
     <div>
       <h1>About Page </h1>
+      <Button>ads</Button>
     </div>
   );
 }
--- packages/ui/atoms/button/button.tsx
+++ packages/ui/atoms/button/button.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import classNames from 'classnames';
 import { ButtonVariant } from './enums';
+import { useForm } from "react-hook-form";
 
 export interface IProps {
   children: string | JSX.Element;
@@ -12,7 +13,7 @@ export interface IProps {
 
 export const Button = ({ children, variant = 'primary', className, onClick, fluid = false }: IProps) => {
   const buttonClasses = classNames(ButtonVariant[variant], 'rounded-lg', { 'w-full': fluid }, className);
-
+  console.log(useForm());
   return (
     <button className={buttonClasses} onClick={onClick}>
       {children}
