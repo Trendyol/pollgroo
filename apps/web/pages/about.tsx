0a36bca13902b4528ce8f5c0d10b2a1a608a7d2b
Armağan Dalkıran <armagandalkran20@gmail.com>
2023-03-18 01:44:48 +0300

    chore: PLGR-45 trigger deployment

--- apps/web/pages/about.tsx
+++ apps/web/pages/about.tsx
@@ -1,12 +1,7 @@
-import { AuthForm } from '@/../../packages/ui';
-
 export default function AboutPage() {
-  const handleSubmit = (data: any) => {
-  };
   return (
     <div>
       <h1>About Page</h1>
-      <AuthForm type="register" onSubmit={handleSubmit} />
     </div>
   );
 }
