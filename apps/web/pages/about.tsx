692f79b207c2023227afe70b8ff5314831affc9a
Armağan Dalkıran <armagandalkran20@gmail.com>
2023-03-18 20:23:09 +0300

    trigger deployment

--- apps/web/pages/about.tsx
+++ apps/web/pages/about.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useForm } from "react-hook-form";
 
 export default function AboutPage() {
-  console.log("useForm --->", useForm());
+  console.log("useForm --->", useForm);
   return (
     <div>
       <h1>About Page</h1>
