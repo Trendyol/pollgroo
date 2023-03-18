1213b24f5598ee6501a61a89005768647fcdea1a
armagan.dalkiran <armagan.dalkiran@trendyol.com>
2023-03-18 17:24:12 +0300

    test commit #2

--- apps/web/pages/about.tsx
+++ apps/web/pages/about.tsx
@@ -1,8 +1,9 @@
 import { Typography } from "@/../../packages/ui";
 import * as yup from "yup";
+import { useForm } from "react-hook-form";
 
 export default function AboutPage() {
-  console.log(yup);
+  console.log(yup, useForm);
   return (
     <div>
       <h1>About Page</h1>
