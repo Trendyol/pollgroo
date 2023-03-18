d900e9c3b2ffd5d64cd2f9b623d399a9d54ce851
armagan.dalkiran <armagan.dalkiran@trendyol.com>
2023-03-18 17:20:35 +0300

    test commit #1

--- apps/web/pages/about.tsx
+++ apps/web/pages/about.tsx
@@ -1,7 +1,12 @@
+import { Typography } from "@/../../packages/ui";
+import * as yup from "yup";
+
 export default function AboutPage() {
+  console.log(yup);
   return (
     <div>
       <h1>About Page</h1>
+      <Typography size="xl" element="p">sd</Typography>
     </div>
   );
 }
