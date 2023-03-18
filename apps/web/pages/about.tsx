d8dd54e10e40ffbbb4cda1816bf3f528e8d0e139
Armağan Dalkıran <armagandalkran20@gmail.com>
2023-03-18 20:38:11 +0300

    trying formik

--- apps/web/pages/about.tsx
+++ apps/web/pages/about.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { useForm } from "react-hook-form";
+import { useFormik } from "formik";
 
 export default function AboutPage() {
   console.log("useForm --->", useForm);
+  console.log("useFormik --->", useFormik);
   return (
     <div>
       <h1>About Page</h1>
