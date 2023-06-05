d414f219563b8e3f54f38f69570708f7360be700
aslihan.ozfidan <aslihan.ozfidan@trendyol.com>
2023-06-05 14:26:56 +0300

    feat: PLGR-58 add popup molecule

--- packages/ui/molecules/index.ts
+++ packages/ui/molecules/index.ts
@@ -3,6 +3,7 @@ export { ScoringButton } from './scoring-button';
 export { GameResultCardScoreBadge } from './gameResultCardScoreBadge';
 export { LabeledDropdown } from './labeledDropdown';
 export { Modal } from './modal';
+export { Popup } from './popup';
 export { ProfileCircle } from './profileCircle';
 export { Loader } from './loader';
 export { EmptyPageWarningBox } from './emptyPageWarningBox';
