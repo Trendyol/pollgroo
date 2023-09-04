79da5973abb6e55ef6acbf205f0bb02b9622073d
Armagan Dalkiran <77741597+armagandalkiran@users.noreply.github.com>
2023-09-04 10:43:51 +0300

    feat: PLGR-167 delete game feature add (#197)

--- apps/web/pages/api/games/[gameId]/index.ts
+++ apps/web/pages/api/games/[gameId]/index.ts
@@ -1,10 +1,10 @@
 import { NextApiResponse } from 'next';
 import connectToDatabase from '@/lib/db';
 import Game from '../../models/game';
+import Team from '../../models/team';
+import Task from '../../models/task';
 import { ExtendedNextApiRequest } from '../../interfaces';
 import { withAuthGameAndTeamMember } from '@/lib/authGameAndTeamMemberMiddleware';
-import('../../models/task');
-import('../../models/team');
 
 async function handler(req: ExtendedNextApiRequest, res: NextApiResponse) {
   const { gameId } = req.query;
@@ -28,6 +28,29 @@ async function handler(req: ExtendedNextApiRequest, res: NextApiResponse) {
       game.isGameMaster = req.userId === game.gameMaster;
 
       res.status(200).json(game);
+    } catch (error) {
+      res.status(500).json({ message: 'Internal server error' });
+    }
+  } else if (req.method === 'DELETE') {
+    try {
+      await connectToDatabase();
+
+      const deletedGame = await Game.findByIdAndDelete(gameId);
+
+      if (!deletedGame) {
+        return res.status(404).json({ message: 'Game not found' });
+      }
+
+      await Task.updateMany(
+        {
+          gameId,
+          score: 0,
+        },
+        { $set: { gameId: null } }
+      );
+      await Team.updateMany({ _id: deletedGame.teamId }, { $pull: { games: gameId } });
+
+      res.status(200).json({isSuccess: true});
     } catch (error) {
       res.status(500).json({ message: 'Internal server error' });
     }
