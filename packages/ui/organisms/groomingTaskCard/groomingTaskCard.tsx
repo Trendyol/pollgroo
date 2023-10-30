6ac081ab17abe758b60e5f7957e1f02e85352585
Burcu Sağlam <burcu.saglam@trendyol.com>
2023-10-30 21:36:51 +0300

    feat: PLGR-188 add description field with accordion ui to grooming task

--- packages/ui/organisms/groomingTaskCard/groomingTaskCard.tsx
+++ packages/ui/organisms/groomingTaskCard/groomingTaskCard.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { IconPencil } from '@tabler/icons-react';
+import { IconChevronDown, IconChevronUp, IconPencil } from '@tabler/icons-react';
 import { Typography } from '../../atoms';
 import { useGrooming } from 'contexts';
 import classNames from 'classnames';
@@ -26,11 +26,27 @@ export const GroomingTaskCard = ({
   className,
 }: IProps) => {
   const { setSelectedTaskToEdit, setShowEditGroomingTaskModal, groomingData } = useGrooming();
+  const [isShowDescription, setIsShowDescription] = React.useState(false);
 
   const handleClick = () => {
     setSelectedTaskToEdit({ _id: taskId, title, description, gameId });
     setShowEditGroomingTaskModal(true);
   };
+
+  const toggleShowDescription = () => {
+    setIsShowDescription(!isShowDescription);
+  }
+
+  const renderAccordionIcon = () => {
+    const chevronIconProps = {
+      className: "text-gray absolute bottom-2 right-2 cursor-pointer",
+      onClick: toggleShowDescription,
+    };
+  
+    const IconComponent = isShowDescription ? IconChevronUp : IconChevronDown;
+  
+    return <IconComponent {...chevronIconProps} />;
+  }
 
   if (groomingData.isScrumPoker) {
     return null;
@@ -38,7 +54,7 @@ export const GroomingTaskCard = ({
 
   return (
     <div
-      className={classNames(`relative bg-lightblue py-2 pl-6 pr-10 rounded-2xl flex flex-col gap-y-3 ${className}`)}
+      className={classNames(`relative bg-lightblue py-2 pl-6 pr-6 rounded-2xl flex flex-col gap-y-3 ${className}`)}
     >
       {!!order && !!totalTaskNumber && (
         <Typography element="p" color="silver" size="xxs">
@@ -48,6 +64,12 @@ export const GroomingTaskCard = ({
       <Typography element="p" color="textgray" size="xs" weight="semibold">
         {title}
       </Typography>
+      {disableEdit && renderAccordionIcon()}
+      {disableEdit && isShowDescription && (
+        <Typography element="p" color='textgray' size="xxs" weight="semibold" className='py-5 border-t border-lightgray'>
+          {description}
+        </Typography>
+      )}
       {!disableEdit && (
         <IconPencil className="absolute top-2 right-2 text-silver w-5 h-5 lg:cursor-pointer" onClick={handleClick} />
       )}
